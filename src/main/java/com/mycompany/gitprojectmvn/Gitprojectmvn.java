7a81cd53ca68c76b4a4552b7f869b01fd5670573
IlyaTomskikh <ilya.tomskih@gmail.com>
2022-11-09 13:02:54 +0700

    broken version

--- src/main/java/com/mycompany/gitprojectmvn/Gitprojectmvn.java
+++ src/main/java/com/mycompany/gitprojectmvn/Gitprojectmvn.java
@@ -22,6 +22,259 @@ public class Gitprojectmvn {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
+        System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gitprojectmvn;
+
+/**
+ *
+ * @author Ilya
+ */
+public class Gitprojectmvn {
+    
+    public static String hello() {
+        return "Hello!!!";
+    }
+    
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        i i i i i ii i ii i i ii i i ii i ii i i i i i i
         System.out.println("Hello World!" + hello() + " " + a + " + " + b + " = " + sum(a, b));
     }
 }
